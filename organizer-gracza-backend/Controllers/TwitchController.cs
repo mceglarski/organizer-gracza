9489b9ace34d7f573cc97cdeace919e8d6cb00d8
DanielJanus <danieljanus100@gmail.com>
2021-12-21 13:52:35 +0100

    Added fix for TwitchApi

--- organizer-gracza-backend/Controllers/TwitchController.cs
+++ organizer-gracza-backend/Controllers/TwitchController.cs
@@ -34,5 +34,28 @@ namespace organizer_gracza_backend.Controllers
                 return BadRequest("Error getting twitch response" + e.StatusCode);
             }
         }
+        
+        [HttpGet("game/{id}")]
+        public async Task<IActionResult> GetDataForGame(string id)
+        {
+            using HttpClient twitch = new HttpClient();
+            try
+            {
+                twitch.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", "wh6c1oa2kkqp33h1bjziw93no30761");
+                twitch.DefaultRequestHeaders.Add("Client-Id", "gp762nuuoqcoxypju8c569th9wz7q5");
+                var response = await twitch.GetAsync($"https://api.twitch.tv/helix/streams/?game_id={id}");
+                response.EnsureSuccessStatusCode();
+
+                string result = await response.Content.ReadAsStringAsync();
+                var json = JsonConvert.DeserializeObject(result);
+
+                return Ok(json);
+            }
+            catch (HttpRequestException e)
+            {
+                return BadRequest("Error getting twitch response" + e.StatusCode);
+            }
+        }
     }
 }
