65124ba3f44ebd5e12a7978397f899e984522349
Piotr Białek <xhunterpb@gmail.com>
2021-12-27 10:02:35 +0100

    added 'forgot password' form, changed event list styles

--- organizer-gracza-frontend/src/app/login/login.component.ts
+++ organizer-gracza-frontend/src/app/login/login.component.ts
@@ -3,6 +3,8 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {AccountService} from "../_services/account.service";
 import {ToastrService} from "ngx-toastr";
 import {Router} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {MembersService} from "../_services/members.service";
 
 
 @Component({
@@ -12,25 +14,48 @@ import {Router} from "@angular/router";
 })
 @Injectable()
 export class LoginComponent implements OnInit {
+
+  public resetPasswordForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, this.noWhitespaceValidator, Validators.email]),
+  })
   model: any = {};
 
-  constructor(private modalService: NgbModal, public accountService: AccountService,
-              private toastr: ToastrService, private router: Router) {}
+  constructor(public accountService: AccountService,
+              private memberService: MembersService,
+              private modalService: NgbModal,
+              private toastr: ToastrService,
+              private router: Router) {}
 
   ngOnInit(): void {
   }
 
-  open(content: any) {
+  public open(content: any): void {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
   }
 
-  login(){
+  public login(): void {
     this.accountService.login(this.model).subscribe(response => {
+      window.location.reload();
+    });
+  }
+
+  public logout(): void{
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+  }
+
+  public sendResetPassword(): void {
+    console.log(this.resetPasswordForm);
+    this.memberService.sendLink(this.resetPasswordForm.value.email).subscribe(response => {
+      this.toastr.success('Link do przywrócenia hasła został wysłany');
+    }, error => {
+      this.toastr.error('Coś poszło nie tak');
     })
   }
 
-  logout(){
-    this.accountService.logout();
-    this.router.navigateByUrl('/');
+  private noWhitespaceValidator(control: FormControl) {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { 'whitespace': true };
   }
 }
